22e01ccfd8d59966431d7a8af1047c9fea7b60f2
tahmim01 <54732116+tahmim01@users.noreply.github.com>
2019-11-06 01:42:22 +0600

    Add files via upload

---
+++ school billing system with basic c/school billing.cpp
@@ -0,0 +1,370 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <math.h>
+
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+     int b,d,e,sum=b+d+e,c,l,due;
+
+      string name;
+
+      int id[10];
+
+      cout<< "<<<****Welcome to school billing system****>>>"<<endl;
+         cout<<" \n Enter student name : ";
+        std::getline( std::cin,name);
+         cout<<"\n thank you for going to pay."<<name<<" enter your id: ";
+         cin>>id[10];
+         cout<<"\n enter your class in digit = ";
+         cin>>c;
+
+         switch(c){
+
+         {
+
+         case 1 :
+
+           cout << "\n Exam fees:1000 ";
+
+          b=1000;
+
+          cout<<"\n write your transport bill:  ";
+
+          cin>>d;
+
+        cout<<"\n school fees   :1800 ";
+
+          e=1800;
+           sum=b+d+e;
+
+
+     cout<< "\n your bill for this month is :" <<sum;
+
+
+       cout << "\n how much you want to pay :";
+        cin>>l;
+        cout<<"\nyour due is:";
+         due=sum-l;
+        cout<<due;
+
+
+       break ;
+
+       }
+
+
+         {
+
+         case 2 :
+
+           cout << "\n Exam fees:1000 ";
+
+          b=1000;
+
+          cout<<"\n transport bill:  ";
+
+          cin>>d;
+
+        cout<<"\n school fees   :1800 ";
+
+          e=1800;
+           sum=b+d+e;
+
+
+     cout<< "\n your bill for this month is :" <<sum;
+
+
+       cout << "\n how much you want to pay :";
+        cin>>l;
+        cout<<"\nyour due is:";
+         due=sum-l;
+        cout<<due;
+
+
+       break ;
+
+       }
+{
+
+         case 3 :
+
+           cout << "\n Exam fees:1000 ";
+
+          b=1000;
+
+          cout<<"\n transport bill:  ";
+
+          cin>>d;
+
+        cout<<"\n school fees   :1800 ";
+
+          e=1800;
+           sum=b+d+e;
+
+
+     cout<< "\n your bill for this month is :" <<sum;
+
+
+       cout << "\n how much you want to pay :";
+        cin>>l;
+        cout<<"\nyour due is:";
+         due=sum-l;
+        cout<<due;
+
+
+       break ;
+
+       }
+
+         {
+
+         case 4 :
+
+            cout << "\n Exam fees:1000 ";
+
+          b=1000;
+
+          cout<<"\n transport bill:  ";
+
+          cin>>d;
+
+        cout<<"\n school fees   :1800 ";
+
+          e=1800;
+
+           sum=b+d+e;
+
+
+     cout<< "\n your bill for this month is :" <<sum;
+
+
+       cout << "\n how much you want to pay :";
+        cin>>l;
+        cout<<"\nyour due is:";
+         due=sum-l;
+        cout<<due;
+
+
+       break ;
+
+       }
+{
+
+         case 5 :
+
+          cout << "\n Exam fees:1000 ";
+
+          b=1000;
+
+          cout<<"\n transport bill:  ";
+
+          cin>>d;
+
+        cout<<"\n school fees   :1800 ";
+
+          e=1800;
+           sum=b+d+e;
+
+
+     cout<< "\n your bill for this month is :" <<sum;
+
+
+       cout << "\n how much you want to pay :";
+        cin>>l;
+        cout<<"\nyour due is:";
+         due=sum-l;
+        cout<<due;
+
+
+       break ;
+
+       }
+       {
+
+         case 6 :
+
+          cout << "\n Exam fees:1000 ";
+
+          b=1000;
+
+          cout<<"\n transport bill:  ";
+
+          cin>>d;
+
+        cout<<"\n school fees   :2500 ";
+
+          e=2500;
+           sum=b+d+e;
+
+
+     cout<< "\n your bill for this month is :" <<sum;
+
+
+       cout << "\n how much you want to pay :";
+        cin>>l;
+        cout<<"\nyour due is:";
+         due=sum-l;
+        cout<<due;
+
+
+       break ;
+
+       }
+     {
+
+         case 7 :
+
+cout << "\n Exam fees:1000 ";
+
+          b=1000;
+
+          cout<<"\n transport bill:  ";
+
+          cin>>d;
+
+        cout<<"\n school fees   :2500 ";
+
+          e=2500;
+           sum=b+d+e;
+
+
+     cout<< "\n your bill for this month is :" <<sum;
+
+
+       cout << "\n how much you want to pay :";
+        cin>>l;
+        cout<<"\nyour due is:";
+         due=sum-l;
+        cout<<due;
+
+
+       break ;
+
+       }
+
+       {
+
+         case 8 :
+
+        cout << "\n Exam fees:1000 ";
+
+          b=1000;
+
+          cout<<"\n transport bill:  ";
+
+          cin>>d;
+
+        cout<<"\n school fees   :2500 ";
+
+          e=2500;
+           sum=b+d+e;
+
+
+     cout<< "\n your bill for this month is :" <<sum;
+
+
+       cout << "\n how much you want to pay :";
+        cin>>l;
+        cout<<"\nyour due is:";
+         due=sum-l;
+        cout<<due;
+
+
+       break ;
+
+       }
+
+{
+
+         case 9 :
+
+         cout << "\n Exam fees:1000 ";
+
+          b=1000;
+
+          cout<<"\n transport bill:  ";
+
+          cin>>d;
+
+        cout<<"\n school fees   :2500 ";
+
+          e=2500;
+           sum=b+d+e;
+
+
+     cout<< "\n your bill for this month is :" <<sum;
+
+
+       cout << "\n how much you want to pay :";
+        cin>>l;
+        cout<<"\nyour due is:";
+         due=sum-l;
+        cout<<due;
+
+
+       break ;
+
+       }
+
+ {
+
+         case 10 :
+
+           cout << "\n Exam fees:1000 ";
+
+          b=1000;
+
+          cout<<"\n transport bill:  ";
+
+          cin>>d;
+
+        cout<<"\n school fees   :2500 ";
+
+          e=2500;
+           sum=b+d+e;
+
+
+     cout<< "\n your bill for this month is :" <<sum;
+
+
+       cout << "\n how much you want to pay :";
+        cin>>l;
+        cout<<"\nyour due is:";
+         due=sum-l;
+        cout<<due;
+
+
+       break ;
+
+       }
+
+
+
+         }
+
+
+
+
+
+
+
+       cout<<"\n *Thank for you to pay your bill \n if there have any due then clear it as early as possible*" <<endl;
+
+
+
+    return 0;
+
+}
+
+
+
+
+
